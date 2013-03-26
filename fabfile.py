350fa853f3958720511357e97bd1251e11ed3f60
Andrew Mleczko <andrew@mleczko.net>
2013-03-26 16:29:58 +0100

    co

--- fabfile.py
+++ fabfile.py
@@ -25,11 +25,33 @@ def production():
               green('penelope.production.server = example.com\n') + \
               green('penelope.production.user = user\n') + \
               green('penelope.production.dir = /opt/my_dir'))
+
+    env.directory = env['penelope.production.dir']
     env.hosts = [env['penelope.production.server']]
     env.user = env['penelope.production.user']
 
-def deploy():
-    with cd(env['penelope.production.dir']):
+def staging():
+    if not 'penelope.staging.server' in env \
+            or not 'penelope.staging.dir' in env \
+            or not 'penelope.staging.user' in env:
+        abort('You don\'t have proper staging ENV. '
+              'Please check your ~/.fabricrc '
+              'and ensure that you have two entries:\n' + \
+              green('penelope.staging.server = example.com\n') + \
+              green('penelope.staging.user = user\n') + \
+              green('penelope.staging.dir = /opt/my_dir'))
+
+    env.directory = env['penelope.staging.dir']
+    env.hosts = [env['penelope.staging.server']]
+    env.user = env['penelope.staging.user']
+
+def co(package, branch='master'):
+    """git checkout [branch] of a specific [package]"""
+    with cd('%s/src/%s' % (env.directory, package)):
+        run('git checkout %s' % branch)
+
+def buildout():
+    with cd(env.directory):
         run("git pull")
         run("./bin/buildout -NU")
         run("touch parts/mod_wsgi/wsgi")
