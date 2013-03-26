49abd02c9521eb8c9335124b4e30e7e68bc961bc
Andrew Mleczko <andrew@mleczko.net>
2013-03-26 14:07:56 +0100

    update fab

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,6 @@
-from fabric.api import lcd, local
+from fabric.api import lcd, local, run, cd, env
+from fabric.utils import abort
+from fabric.colors import green
 
 
 def release(package):
@@ -9,3 +11,22 @@ def release(package):
         version = local('../../bin/python setup.py --version', capture=True).strip()
         local('../../bin/postrelease')
     local(u"sed -i 's/\(%s = \)\(.*\)$/%s%s/g' versions/production.cfg" % (package, r'\1', version))
+    local("git add versions/production.cfg")
+    local("git commit -m 'update production versions for %s'" % package)
+    local("git push")
+
+def production():
+    if not 'penelope.production.server' in env \
+            or 'penelope.production.dir' in env:
+        abort('You don\'t have proper production ENV. '
+              'Please check your ~/.fabricrc '
+              'and ensure that you have two entries:\n' + \
+              green('penelope.production.server = example.com\n') + \
+              green('penelope.production.dir = /opt/my_dir'))
+    env.hosts = env['penelope.production.server']
+
+def deploy():
+    with cd(env['penelope.production.dir']):
+        run("git pull")
+        run("./bin/buildout -NU")
+        run("touch parts/mod_wsgi/wsgi")
