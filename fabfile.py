cace50a3e5abf02a5853df4cf93a95604fac762e
Andrew Mleczko <andrew@mleczko.net>
2013-03-27 11:51:16 +0100

    improvements on the fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,6 @@
-from fabric.api import lcd, local, run, cd, env
+from fabric.api import lcd, local, run, cd, env, quiet
 from fabric.utils import abort
-from fabric.colors import green
+from fabric.colors import green, red
 
 def release(package):
     """ release [package] on pypi and update versions.cfg """
@@ -15,6 +15,7 @@ def release(package):
     local("git push")
 
 def production():
+    """Use production server"""
     if not 'penelope.production.server' in env \
             or not 'penelope.production.dir' in env \
             or not 'penelope.production.user' in env:
@@ -29,7 +30,13 @@ def production():
     env.hosts = [env['penelope.production.server']]
     env.user = env['penelope.production.user']
 
+def dev():
+    """Use local development server"""
+    with quiet():
+        env.directory = local('pwd', capture=True)
+
 def staging():
+    """Use staging server"""
     if not 'penelope.staging.server' in env \
             or not 'penelope.staging.dir' in env \
             or not 'penelope.staging.user' in env:
@@ -44,21 +51,52 @@ def staging():
     env.hosts = [env['penelope.staging.server']]
     env.user = env['penelope.staging.user']
 
-def co(package, branch='master'):
-    """{remote} git checkout [branch] of a specific [package]"""
-    with cd('%s/src/%s' % (env.directory, package)):
-        run("git checkout master", quiet=True)
-        run("git pull", quiet=True)
-        run('git checkout %s' % branch)
+def _co(package, branch, _cd, _run, **kwargs):
+    """git checkout [branch] of a specific [package]"""
+    packages_dir = '%s/src' % env.directory
+    with _cd(packages_dir):
+        if not _run('ls -d * | grep %s | cat' % package, **kwargs):
+            print red('package [%s] not found' % package)
+            return
+    with _cd('%s/%s' % (packages_dir, package)):
+        if not _run('git branch -r | grep %s | cat' % branch, **kwargs):
+            print red('[%s] not found for package [%s]' % (branch, package))
+            return
+        print green('checkout [%s] for package [%s]' % (branch, package))
+        _run("git checkout master")
+        _run("git pull")
+        _run('git checkout %s' % branch)
 
-def lco(package, branch='master'):
-    """{local} git checkout [branch] of a specific [package]"""
-    with lcd('src/%s' % package):
-        local("git checkout master", capture=True)
-        local("git pull", capture=True)
-        local('git checkout %s' % branch)
+def co(package, branch='master'):
+    """git checkout [branch] of a specific [package]"""
+    if not 'directory' in env:
+        abort('Usage: $ fab [production|staging|dev] co:package_name')
+    with quiet():
+        if env.hosts:
+            _co(package, branch, cd, run, quiet=True)
+        else:
+            _co(package, branch, lcd, local, capture=True)
+
+def _pullower(branch, _cd, _run, **kwargs):
+    packages_dir = '%s/src' % env.directory
+    with _cd(packages_dir):
+        output = _run('ls -d *', **kwargs)
+        packages = output.split()
+    for package in packages:
+        _co(package, branch, _cd, _run, **kwargs)
+
+def pullower(branch='master'):
+    """ tries to pull [branch] from all sources """
+    if not 'directory' in env:
+        abort('Usage: $ fab [production|staging|dev] pullrequest:branch')
+    with quiet():
+        if env.hosts:
+            _pullower(branch, cd, run, quiet=True)
+        else:
+            _pullower(branch, lcd, local, capture=True)
 
 def buildout():
+    """Pull buildout, run bin/buildount, touch wsgi"""
     with cd(env.directory):
         run("git pull")
         run("./bin/buildout -NU")
