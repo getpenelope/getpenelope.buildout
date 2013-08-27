cefe5a5eb6d09609aa05a32566dd315df5e923f6
Andrew Mleczko <andrew@mleczko.net>
2013-08-27 19:43:46 +0200

    update migration script for kanban

--- alembic/versions/275c8091ee37_kanbanboard.py
+++ alembic/versions/275c8091ee37_kanbanboard.py
@@ -37,37 +37,23 @@ def upgrade():
                     sa.Column('project_id', sa.String(), sa.ForeignKey('projects.id', ondelete="CASCADE")),
                     sa.Column('kanban_id', sa.Integer(), sa.ForeignKey('kanban_boards.id', ondelete="CASCADE")),
                     )
+    op.add_column('kanban_boards', sa.Column('backlog_limit', sa.Integer(), nullable=False))
+    op.add_column('kanban_boards', sa.Column('backlog_order', sa.Unicode(), nullable=False))
+    op.add_column('kanban_boards', sa.Column('backlog_query', sa.Unicode(), nullable=True))
+    op.drop_column('kanban_boards', u'board_query')
+    op.alter_column('kanban_boards', u'name',
+               existing_type=sa.VARCHAR(),
+               nullable=False)
 
-    op.create_table(
-                    'kanban_acl',
-                    sa.Column('id', sa.Integer(), primary_key=True, nullable=False),
-                    sa.Column('board_id', sa.Integer(), sa.ForeignKey('kanban_boards.id', ondelete="CASCADE"), nullable=False),
-                    sa.PrimaryKeyConstraint(u'id', name=u'kanban_acl_pkey')
-                    )
-
-    op.alter_column('kanban_acl', u'id', 
-               existing_type=sa.INTEGER(), 
-               type_=sa.Integer(),
-               existing_nullable=False, 
-               existing_server_default='''nextval('kanban_acl_id_seq'::regclass)''')
-
-    op.create_table(
-                    'kanban_acl_principals',
-                    sa.Column('id', sa.Integer(), primary_key=True, nullable=False),
-                    sa.Column('principal', sa.String(), nullable=False),
-                    sa.Column('permission_name', sa.String(), nullable=False),
-                    sa.Column('kanban_acl_id', sa.Integer(), sa.ForeignKey('kanban_acl.id', ondelete="CASCADE"), nullable=False),
-                    sa.PrimaryKeyConstraint(u'id', name=u'kanban_acl_principals_pkey')
-                    )
-
-    op.alter_column('kanban_acl_principals', u'id', 
-               existing_type=sa.INTEGER(), 
-               type_=sa.Integer(),
-               existing_nullable=False, 
-               existing_server_default='''nextval('kanban_acl_principals_id_seq'::regclass)''')
+    op.create_table('kanban_acl',
+            sa.Column('id', sa.Integer(), nullable=False),
+            sa.Column('principal', sa.String(), nullable=True),
+            sa.Column('permission_name', sa.String(), nullable=True),
+            sa.Column('board_id', sa.Integer(), sa.ForeignKey('kanban_boards.id', ondelete="CASCADE")),
+            sa.PrimaryKeyConstraint('id')
+            )
 
 def downgrade():
-    op.drop_table('kanban_acl_principals')
     op.drop_table('kanban_acl')
     op.drop_table('kanban_projects')
     op.drop_table('kanban_boards')
